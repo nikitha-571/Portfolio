f53a56e0980fee831a3e1dd26f0f65dbaf467b7a
Nikitha devaraj <106416190+nikitha-571@users.noreply.github.com>
2023-07-12 10:40:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Cloud Architect", "Aspiring Student"],
+        strings: ["Engineer", "Aspiring Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
